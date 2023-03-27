782ecc4ebca1ccef11c97212d85ad496c68c1eb0
Gabriel Verde <gabriel.verde19ea@cenidet.edu.mx>
2023-03-26 20:32:09 -0600

    Modificación de cuenta
    darse cuenta de la relación entre clases a través de la composición;
    
    • Ventajas de aislar información repetida en otra clase;
    
    • Detalles de la composición;
    
    • NullPointerException para atributos no inicializados y su cuidado.

--- src/bytebank/Cuenta.java
+++ src/bytebank/Cuenta.java
@@ -5,6 +5,29 @@ public class Cuenta {
     double saldo;
     int agencia;
     int numero;
-    String titular;
+    Cliente titular;
+
+    public void deposita(double valor) {
+        this.saldo = this.saldo + valor;
+    }
+
+    public boolean saca(double valor) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean transfiere(double valor, Cuenta destino) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.deposita(valor);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
